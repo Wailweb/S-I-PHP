aebc8864d502cebd2d4a1ef6c848ae5348469bd3
Wailweb <wail.sdni@gmail.com>
2020-04-03 14:18:30 +0200

    commit cookies

--- entete.php
+++ entete.php
@@ -1,5 +1,7 @@
-<header>
+<?php
+// on définit une durée de vie de notre cookie (en secondes), donc un an dans notre cas
+$temps = 365*24*3600;
 
-    <h1>coucou</h1>
-
-</header>
+// on envoie un cookie de nom pseudo portant la valeur LA GLOBULE
+setcookie ("pseudo", "LA GLOBULE", time() + $temps);
+?>
